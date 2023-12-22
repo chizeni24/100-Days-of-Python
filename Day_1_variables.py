58926d418eb68b9aa5e0b92f8f09750922f37c5a
chizeni24 <zvy431@my.utsa.edu>
2023-12-22 01:46:00 -0600

    First Day

---
+++ Day_1_variables.py
@@ -0,0 +1,11 @@
+# 1. Create a greeting for your program
+print("Welcome to the band Generator.\n")
+
+# 2. Ask the user for the cuty that they grew up in
+city = input("What city did you grow up in? \n")
+
+# 3. Ask the user for the name of their pet
+pet = input("What is the name of your pet? \n")
+
+# Combine the name of their city and pet and show them thier band name.
+print(f"The name of your band is {city} {pet} \n")
